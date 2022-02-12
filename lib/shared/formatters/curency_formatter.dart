0c5805de9b42395ef95dacf7a04e0fdd4729bb07
Fellipe Luz Souza Machado <fellipe.luz.machado@gmail.com>
2022-02-12 00:36:58 +0000

    implemented curency format field

--- lib/shared/formatters/curency_formatter.dart
+++ lib/shared/formatters/curency_formatter.dart
@@ -3,38 +3,43 @@ import 'dart:html';
 import 'package:flutter/services.dart';
 
 class CurencyFormatter extends TextInputFormatter {
-  final String decimalSeparator = ',';
-  final String thousandSeparator = '.';
-
-  final String sampleValue = 'x,xx';
 
   @override
   TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
-    if (newValue.text.length < sampleValue.length) {
-      if (newValue.text.isEmpty){
-        return TextEditingValue(
-          text: '0,00',
-          selection: TextSelection.collapsed(offset: newValue.selection.end + 1),
-        );
-      }
-      if (newValue.text.length == 1){
-        return TextEditingValue(
-          text: '0,0${newValue.text}',
-          selection: TextSelection.collapsed(offset: newValue.selection.end + 1),
-        );
-      }
-      if (newValue.text.length == 2){
-        return TextEditingValue(
-          text: '0,${newValue.text}',
-          selection: TextSelection.collapsed(offset: newValue.selection.end + 1),
-        );
-      }
-      if (newValue.text.length > ) 
-    }
+    final value;
+    if (newValue.text.isEmpty) value = 0;
+    else value = newValue.text;
 
-    return oldValue;
+    return TextEditingValue(text: formatedValue(intValue(value)));
+  }
+
+  int intValue(String value) {
+    return int.parse(value.replaceAll(RegExp(r'[^0-9]')));
+  }
+  
+  String formatedValue(int value){
+    String decimal = (value % 100).toString();
+    if (decimal.length < 2) decimal = '0' + decimal;
+    value = (value / 100).toInt();
+
+    String strValue = value.toString();
+    String reversed = strValue.reverse();
+    String grouped = reversed.replaceAllMapped(RegExp(r".{3}"), (match) => "${match.group(0)} ").trim();
+    String dotted = grouped.replaceAll(RegExp(r' '), '.');
+    String integer = dotted.reverse();
+
+    return '$integer,$decimal';
   }
 
 
-
+  extension Reverse on String {
+    String reverse() {
+      var reversed = '';
+      for (var i = 0; i < this.length; i++){
+        reversed += this[this.length - i - 1];
+      }
+      return reversed;
+    }
+  }
 }
+
